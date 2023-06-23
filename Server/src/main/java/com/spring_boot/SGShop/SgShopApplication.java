e42e66f65322eeb010d6a096e78ca7022530ac82
sondev_99 <transon220599@gmail.com>
2023-06-23 12:22:55 +0700

    first commit

---
+++ Server/src/main/java/com/spring_boot/SGShop/SgShopApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot.SGShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgShopApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/com/spring_boot/SGShop/SgShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot.SGShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
